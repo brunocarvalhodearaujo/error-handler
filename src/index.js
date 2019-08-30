95d6011657da8e172c0948924a43dde4b13b68ee
Bruno Carvalho de Araujo <bruno@Brunos-MacBook-Pro.local>
2019-08-30 17:00:29 -0300

    field trace_id

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function middleware (err, request, response, next) {
   }
 
   // client errors
-  assign(error, pick(err, [ 'message', 'name', 'code', 'type' ]))
+  assign(error, pick(err, [ 'message', 'name', 'code', 'type', 'trace_id' ]))
 
   response.json({ error })
 }
